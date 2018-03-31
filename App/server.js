c615d978b3a4903a5dcea7587afd8eb51ad71fe7
linux17kartik <linux.kartik@gmail.com>
2018-03-31 12:07:44 +0530

    First commit for test1

---
+++ App/server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express');
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  port : 3306,
+  user     : 'root',
+  password : 'mysql#123',
+  database : 'helloworld' 
+});
+ 
+connection.connect();
+
+// Constants
+const PORT = 80;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log(`Running on http://:${PORT}`);
+
+ 
+connection.query('SELECT * from pet', function (error, results, fields) {
+  if (error){
+	console.log(error);	
+	}
+  console.log('The solution is: ', results);
+});
+ 
+connection.end();
